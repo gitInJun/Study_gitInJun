68c04e1389622437802bbed54c4f6ceb175bb3a9
dlswns6532 <hij092919@gmail.com>
2022-11-11 14:47:43 +0900

    block 기능 써보기

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/for")
 def hello():
-    return render_template('test.j2', title = 'Hello jinja!')
+    return render_template('index.j2', title = 'Hello jinja!')
+
+@app.route("/")
+def test():
+    return render_template('SoCowNet.j2', title = 'SO COWNET')
 
 if __name__ == '__main__':
     app.run()
