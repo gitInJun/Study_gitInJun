a612c0fa48cae8dc759b544ecce8d603f6e63eab
dlswns6532 <hij092919@gmail.com>
2022-11-14 15:19:47 +0900

    인풋 태그 내부에서의 파이썬 변수 사용 가능 여부 확인

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, url_for
+import datetime as dt
+
 app = Flask(__name__)
 
 @app.route("/")
-def test():
-    return render_template('main.j2')
+def mian():
+    today=str(dt.datetime.today())[:10]
+    return render_template('main.j2', today=today, a=1)
 
 if __name__ == '__main__':
     app.run()
