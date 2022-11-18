e49bc5b865e487b7d66546fbd61d38a5e73e76ad
dlswns6532 <hij092919@gmail.com>
2022-11-18 17:56:20 +0900

    차트 업데이트 구현

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     today = date.today()
-    earlyMonth = date(today.year, today.month, 1)
-    dayLabel = list(range(1, (today - earlyMonth).days + 2))
-    randomlist = [random.choices(range(100, 200), k=(today - earlyMonth).days + 1), random.choices(range(100, 200), k=(today - earlyMonth).days + 1), random.choices(range(100, 200), k=(today - earlyMonth).days + 1), random.choices(range(100, 200), k=(today - earlyMonth).days + 1), random.choices(range(100, 200), k=(today - earlyMonth).days + 1),]
-    return render_template('main.j2', today=today, earlyMonth=earlyMonth, dayLabel=dayLabel, randomlist=randomlist)
+    defaultStart = today - timedelta(29)
+    dayLabel = list(range(1, (today - defaultStart).days + 1))
+    randomlist = [random.choices(range(100, 200), k=(today - defaultStart).days+1), random.choices(range(100, 200), k=(today - defaultStart).days+1), random.choices(range(100, 200), k=(today - defaultStart).days+1), random.choices(range(100, 200), k=(today - defaultStart).days+1), random.choices(range(100, 200), k=(today - defaultStart).days+1),]
+    return render_template('main.j2', today=today, defaultStart=defaultStart, dayLabel=dayLabel, randomlist=randomlist)
 
-@app.route("/mine")
+@app.route("/2")
 def mine():
     return render_template('mine.j2')
 
